0137f350fafc9c82314f3002ef8062f1820105d7
Ceci <165979159+wushg1224@users.noreply.github.com>
2025-10-16 17:22:10 +1100

    dashboard

--- DOTNETA2/Form1.cs
+++ DOTNETA2/Form1.cs
@@ -1,43 +1,102 @@
-using System.Globalization;
-using DOTNETA2.Controller;
-using DOTNETA2.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace DOTNETA2;
-
-public partial class Form1 : Form
+namespace DOTNETA2
 {
-    private TransactionController t = new TransactionController();
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-       
-    }
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-        InitListView();
-    }
-    
-    private void InitListView()
-    {
-        transactionListView.Columns.Add("Date", 300);
-        transactionListView.Columns.Add("Type", 120);
-        transactionListView.Columns.Add("Category", 150);
-        transactionListView.Columns.Add("Amount", 120);
-        LoadTransactions();
-    }
-    
-    private void LoadTransactions()
-    {
-        
-        var au = new CultureInfo("en-AU");
-        foreach (Transaction t in t.ShowTransactions())
+        int panelWidth;
+        bool isCollapsed;
+        public Form1()
         {
-            var item = new ListViewItem($"{t.Date:yyyy-MM-dd HH:mm:ss}");
-            item.SubItems.Add(t.Type.ToString());
-            item.SubItems.Add(t.Category.ToString());
-            item.SubItems.Add(t.Amount.ToString("C",au));
-            transactionListView.Items.Add(item);
+            InitializeComponent();
+            timerTime.Start();
+            panelWidth = panelLeft.Width;
+            isCollapsed = false;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //shutdown application
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCollapsed)
+            {
+                panelLeft.Width = panelLeft.Width + 10;
+                if (panelLeft.Width >= panelWidth)
+                {
+                    timer1.Stop();
+                    isCollapsed = false;
+                    this.Refresh();
+                }
+            }
+            else
+            {
+                panelLeft.Width = panelLeft.Width - 10;
+                if (panelLeft.Width <= 59)
+                {
+                    timer1.Stop();
+                    isCollapsed = true;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //show current time
+            DateTime dt = DateTime.Now;
+            //show in 24 hour format
+            LabelTime.Text = dt.ToString("HH:mm:ss");
+
         }
     }
 }
